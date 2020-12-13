c75473dd7e396b27817c70d933604713f5a5e371
hantsy <hantsy@gmail.com>
2020-12-13 09:43:34 +0800

    install orm

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
