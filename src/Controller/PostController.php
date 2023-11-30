e96c4a72b4031274e448371048c0800a24a47a97
hantsy <hantsy@gmail.com>
2023-11-30 22:38:26 +0800

    chore: new doc

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -18,7 +18,7 @@ use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Uid\Uuid;
 
