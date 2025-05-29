112b25bb8841bbea1211fa50caf15035b4251929
hantsy <hantsy@gmail.com>
2025-05-29 20:07:23 +0800

    chore: update to symfony 7.3

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,10 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
+use Doctrine\Deprecations\Deprecation;
+
+Deprecation::withoutDeduplication();
+
 if (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
